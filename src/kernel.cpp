9d7e3d7cc49efbc54df7aecab6740d03e28c91b3
Andy Wat <lyos1024@gmail.com>
2017-07-30 01:24:05 +0800

    vga support

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -6,6 +6,7 @@
 #include <drivers/driver.h>
 #include <drivers/keyboard.h>
 #include <drivers/mouse.h>
+#include <drivers/vga.h>
 
 using namespace lyos;
 using namespace lyos::common;
@@ -135,11 +136,21 @@ extern "C" void kernelMain(void *multiboot_structure, uint32_t magicnumber)
 	PeripheralComponentInterconectController PCIController;
 	PCIController.SelectDrivers(&drvManager, &interrupts);
 
+	VideoGraphicsArray vga;
+
 	printf("Initializing Hardwae, Stage 2\n");
 	drvManager.ActivateAll();
 
 	printf("Initializing Hardwae, Stage 3\n");
 	interrupts.Activate();
+
+	vga.SetMode(320, 200, 8);
+	for (uint32_t y = 0; y < 200; y++)
+		for (uint32_t x = 0; x < 320; x++)
+		{
+			vga.PutPixel(x, y, 0x00, 0x00, 0xA8);
+		}
+
 	while (1)
 		;
 }
